c2d06cec658f7101035e78dd339a3e84e34a9009
jydnbach <jaydenkim95@gmail.com>
2024-07-03 13:31:40 +0900

    managing through context instead of redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
+// import productReducer from './store/reducers/products';
+import ProductsProvider from './context/products-context';
 
 const rootReducer = combineReducers({
   shop: productReducer,
@@ -16,9 +17,9 @@ const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <ProductsProvider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProductsProvider>
 );
